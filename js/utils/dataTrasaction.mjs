7464434f07a0eb5dee72da31ae791c35f5f30ac9
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-03-09 22:17:14 +0700

    chore: sort modules

--- js/utils/dataTrasaction.mjs
+++ js/utils/dataTrasaction.mjs
@@ -28,6 +28,46 @@
  *
  */
 
+
+/**
+ * Module untuk mengubah tanggal (hh/bb/tttt) dari bentuk string ke Date.
+ *
+ * @param {String} date_str - tanggal berbentuk string. Biasanya digunakan untuk `Transaction.date`
+ * @returns {Date} tanggal yang sudah diformat ke bentuk Date
+ */
+export function parseDate(date_str) {
+  const [day, month, year] = date_str.split("/");
+  return new Date(year, month - 1, day);
+}
+
+/**
+ * Module untuk me-format data dari form ke bentuk object javascript.
+ *
+ * @param {FormData} form - Form data
+ * @param {'spending'|'income'} typeTransaction - Tipe transaksi antara pengeluaran atau pendapatan
+ * @returns {Transaction} Data transaksi data
+ */
+export function parseFormData(form, typeTransaction) {
+  // validate type transaction
+  if (typeTransaction !== "spending" && typeTransaction !== "income") {
+    console.error(
+      "`typeTransaction` tidak valid. Opsi hanya tersedia spending atau income"
+    );
+    return;
+  }
+
+  const parseData = {};
+  // convert form date to type date with dd/mm/yyyy format
+  parseData["date"] = new Date(
+    form.get(`date-${typeTransaction}`)
+  ).toLocaleDateString("en-GB");
+  parseData["amount"] = parseInt(form.get(`amount-${typeTransaction}`));
+  parseData["category"] = form.get(`category-${typeTransaction}`);
+  parseData["desc"] = form.get(`desc-${typeTransaction}`);
+
+  return parseData;
+}
+
 /**
  * Getter untuk mendapatkan username
  *
@@ -71,82 +111,6 @@ export function getIncomeList() {
  */
 export function getOutcomeList() {
   return JSON.parse(window.localStorage.getItem("spending"));
-}
-
-/**
- * Module untuk menambah data pendapatan/pengeluaran transaksi
- *
- * @param {Transaction} newTransaction - data transaksi dari form
- * @param {'spending'|'income'} typeTransaction - Tipe transaksi. Nilai yang valid adalah 'spending' atau 'income'
- */
-export function addNewIncome(newTransaction, typeTransaction) {
-  // validate type transaction
-  if (typeTransaction !== "spending" && typeTransaction !== "income") {
-    console.error(
-      "`typeTransaction` tidak valid. Opsi hanya tersedia spending atau income"
-    );
-    return;
-  }
-
-  // load existing data
-  const existingTransaction =
-    (typeTransaction === "spending" && (getSpendingList() || [])) ||
-    (typeTransaction === "income" && (getIncomeList() || []));
-
-  // append new data to existing data
-  existingTransaction.push(newTransaction);
-
-  // sort by date
-  existingTransaction.sort((a, b) => parseDate(a.date) - parseDate(b.date));
-  console.log(`[SORTED] current ${typeTransaction}`, existingTransaction);
-
-  // save to the local storage
-  typeTransaction === "income" &&
-    window.localStorage.setItem("income", JSON.stringify(existingTransaction));
-  typeTransaction === "spending" &&
-    window.localStorage.setItem(
-      "spending",
-      JSON.stringify(existingTransaction)
-    );
-}
-
-/**
- * Module untuk mengubah tanggal (hh/bb/tttt) dari bentuk string ke Date.
- *
- * @param {String} date_str - tanggal berbentuk string. Biasanya digunakan untuk `Transaction.date`
- * @returns {Date} tanggal yang sudah diformat ke bentuk Date
- */
-export function parseDate(date_str) {
-  const [day, month, year] = date_str.split("/");
-  return new Date(year, month - 1, day);
-}
-
-/**
- * Module untuk me-format data dari form ke bentuk object javascript.
- *
- * @param {FormData} form - Form data
- * @param {'spending'|'income'} typeTransaction - Tipe transaksi antara pengeluaran atau pendapatan
- * @returns {Transaction} Data transaksi data
- */
-export function parseFormData(form, typeTransaction) {
-  // validate type transaction
-  if (typeTransaction !== "spending" && typeTransaction !== "income") {
-    console.error(
-      "`typeTransaction` tidak valid. Opsi hanya tersedia spending atau income"
-    );
-    return;
-  }
-
-  const parseData = {};
-  // convert form date to type date with dd/mm/yyyy format
-  parseData["date"] = new Date(
-    form.get(`date-${typeTransaction}`)
-  ).toLocaleDateString("en-GB");
-  parseData["amount"] = parseInt(form.get(`amount-${typeTransaction}`));
-  parseData["category"] = form.get(`category-${typeTransaction}`);
-  parseData["desc"] = form.get(`desc-${typeTransaction}`);
-
-  return parseData;
 }
 
 /**
@@ -247,6 +211,43 @@ export function getSpendingDataWithCategory() {
     category: key,
     amount: data[key],
   }));
+}
+
+/**
+ * Module untuk menambah data pendapatan/pengeluaran transaksi
+ *
+ * @param {Transaction} newTransaction - data transaksi dari form
+ * @param {'spending'|'income'} typeTransaction - Tipe transaksi. Nilai yang valid adalah 'spending' atau 'income'
+ */
+export function addNewIncome(newTransaction, typeTransaction) {
+  // validate type transaction
+  if (typeTransaction !== "spending" && typeTransaction !== "income") {
+    console.error(
+      "`typeTransaction` tidak valid. Opsi hanya tersedia spending atau income"
+    );
+    return;
+  }
+
+  // load existing data
+  const existingTransaction =
+    (typeTransaction === "spending" && (getSpendingList() || [])) ||
+    (typeTransaction === "income" && (getIncomeList() || []));
+
+  // append new data to existing data
+  existingTransaction.push(newTransaction);
+
+  // sort by date
+  existingTransaction.sort((a, b) => parseDate(a.date) - parseDate(b.date));
+  console.log(`[SORTED] current ${typeTransaction}`, existingTransaction);
+
+  // save to the local storage
+  typeTransaction === "income" &&
+    window.localStorage.setItem("income", JSON.stringify(existingTransaction));
+  typeTransaction === "spending" &&
+    window.localStorage.setItem(
+      "spending",
+      JSON.stringify(existingTransaction)
+    );
 }
 
 /**
