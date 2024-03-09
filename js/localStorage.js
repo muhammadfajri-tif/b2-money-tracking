07dbc0a208bf2f5df5ff389a8a2b768a53b62776
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-03-09 23:47:09 +0700

    feat: create buttons for backup/export data to json

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,6 +1,5 @@
-
-import { loadFileContent, isDataValid, initializeDataToLocalStorage } from './utils/file.mjs';
-import { getUsername, getMoney, getIncomeList, getSpendingList, addNewIncome, parseDate, parseFormData } from './utils/dataTrasaction.mjs'
+import { loadFileContent, isDataValid, initializeDataToLocalStorage, exportUserDataToJSON } from './utils/file.mjs';
+import { getUsername, getMoney, getIncomeList, getSpendingList, addNewIncome, parseDate, parseFormData, getOutcomeList } from './utils/dataTrasaction.mjs'
 
 // handle form file upload untuk import data
 const fileSelector = document.getElementById('file-select'); // must be appropriate
@@ -123,7 +122,62 @@ function handleCreateNewAccount(event) {
     initializeDataToLocalStorage(newAccount);
     alert("user baru berhasil dibuat");
   }
+}
 
+// handle backup/export account
+const backupAccountButton = document.getElementById("backup-account-button");
+backupAccountButton.addEventListener('click', handleBackupAccount);
+
+function handleBackupAccount(event) {
+  // construct object to export
+  const account = {
+    name: getUsername(),
+    money: getMoney(),
+    spending: getSpendingList(),
+    income: getIncomeList(),
+  }
+
+  // create downloadable file
+  const metadataFile = exportUserDataToJSON(account);
+  const fileName = `backup-${account.name}_money-tracking-by-b2.json`;
+
+  // manipulate DOM to enable download file
+  backupAccountButton.setAttribute('href', metadataFile.url);
+  backupAccountButton.setAttribute('download', fileName);
+}
+
+// handle backup/export income
+const exportIncomeUserButton = document.getElementById("export-income-json-button");
+exportIncomeUserButton.addEventListener('click', handleExportIncome);
+
+function handleExportIncome() {
+  // get data
+  const data = getIncomeList();
+
+  // create downloadable file
+  const metadataFile = exportUserDataToJSON(data);
+  const fileName = `${getUsername()}_income-data_money-tracking-by-b2.json`;
+
+  // manipulate DOM to enable download file
+  exportIncomeUserButton.setAttribute('href', metadataFile.url);
+  exportIncomeUserButton.setAttribute('download', fileName);
+}
+
+// handle backup/export spending
+const exportSpendingUserButton = document.getElementById("export-spending-json-button");
+exportSpendingUserButton.addEventListener('click', handleExportSpending);
+
+function handleExportSpending() {
+  // get data
+  const data = getSpendingList();
+
+  // create downloadable file
+  const metadataFile = exportUserDataToJSON(data);
+  const fileName = `${getUsername()}_spending-data_money-tracking-by-b2.json`;
+
+  // manipulate DOM to enable download file
+  exportSpendingUserButton.setAttribute('href', metadataFile.url);
+  exportSpendingUserButton.setAttribute('download', fileName);
 }
 
 
